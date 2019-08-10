7d1ee1d7af66a43a5a8b0fc44e782b6059678f99
jcosentino <johnsc301@gmail.com>
2019-08-09 21:24:40 -0400

    Added basic login pieces, along with some components for future pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,45 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { UserComponent } from './user/user.component';
+import { ProfileComponent } from './profile/profile.component';
+import { AppointmentComponent } from './appointment/appointment.component';
+import { InsuranceComponent } from './insurance/insurance.component';
+import { DxCheckBoxModule,
+  DxSelectBoxModule,
+  DxNumberBoxModule,
+  DxButtonModule,
+  DxFormModule,
+  DxAutocompleteModule,
+  DxValidatorComponent,
+  DxTextBoxComponent,
+  DxDateBoxComponent,
+  DxValidationSummaryComponent} from 'devextreme-angular';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+    UserComponent,
+    ProfileComponent,
+    AppointmentComponent,
+    InsuranceComponent,
+    DxValidatorComponent,
+    DxTextBoxComponent,
+    DxDateBoxComponent,
+    DxValidationSummaryComponent
   ],
   imports: [
+    DxCheckBoxModule,
+    DxSelectBoxModule,
+    DxNumberBoxModule,
+    DxButtonModule,
+    DxFormModule,
+    DxAutocompleteModule,
     BrowserModule,
     AppRoutingModule
   ],
