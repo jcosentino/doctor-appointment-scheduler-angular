4e6afa5b819ab7c582ec913454ff53d75b52eef5
John Cosentino <johnsc301@gmail.com>
2019-11-24 16:43:14 -0500

    [11/24/2019] - 4:43pm - restarted the application code base

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to doctor-appointment-scheduler-angular!');
+    expect(page.getTitleText()).toEqual('doctor-app app is running!');
   });
 
   afterEach(async () => {
--- src/assets/constants/app_constants/app_constants.ts
+++
@@ -1,4 +0,0 @@
-export class AppConstants {
-    public static API_ENDPOINT = 'http://127.0.0.1:5000/';
-    public static APP_PAGE_GREETING = 'Welcome to the doctor appointment scheduler!';
- }
